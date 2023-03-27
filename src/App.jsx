62535e8746693bd48ea2c073c5a949d0e80cba03
emabistar <emabi2015@gmail.com>
2023-03-27 23:24:14 +0200

    Todo-react-vite-typescript

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import './App.css'
+import { Button, Card, Form } from 'react-bootstrap';
+
+import Todo from "./componets/Todo"
+import FormTodo  from "./componets/FormTodo"
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "This is a sampe todo",
+      isDone: false
+    }
+
+  ]);
+  const markTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isDone = true;
+    setTodos(newTodos);
+};
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+};
+const removeTodo= index =>{
+ const newTodos =[...todos];
+ newTodos.splice(index,1)
+setTodos(newTodos)
+}
+
+
+  return (
+    <div className="app">
+      <div className="container">
+        <h1 className="text-center mb-4">Todo List</h1>
+         <FormTodo addTodo={addTodo} />
+        <div>
+          {todos.map((todo, index) => (
+            <Card>
+              <Card.Body>
+                <Todo
+                key={index}
+                index={index}
+                todo={todo}
+                markTodo={markTodo}
+                removeTodo={removeTodo}
+                />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
